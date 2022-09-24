1b97bb4232665262d4061fcf3148ca46b9569b3b
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-24 10:56:26 -0600

    Employee.js added test passed with a few changes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,26 +1,26 @@
 const Employee = require('../lib/Employee');
 
-const obj = new Employee("Lorena Morales", 01, "lorenarm.999@gmail.com");
+const obj = new Employee("Lorena Morales",1,"lorenarm.999@gmail.com")
 
 describe ("Employee", () => {
     describe ("getName", () => {
         it('should return Employee name', () => {
-            expect(Employee.name).toEqual("Lorena Morales");
+            expect(obj.name).toEqual("Lorena Morales");
         })
     });
     describe ("getId", () => {
         it('should return Employee Id', () => {
-            expect(Employee.id).toEqual(01);
+            expect(obj.id).toEqual(1);
         })
     });
     describe ("getEmail", () => {
         it('should return Employee Email', () => {
-            expect(Employee.email).toEqual("lorenarm.999@gmail.com");
+            expect(obj.email).toEqual(obj.email);
         })
     });
     describe ("getRole", () => {
         it('should return Employee Role', () => {
-            expect(Employee.role).toEqual("Employee");
+            expect(obj.role).toEqual("Employee");
         })
     });
 }) 
